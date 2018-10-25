653e01c0dd61b97c6a5b9be8bd1f97623e77240a
Agung Pribadi <omegaudayana777@gmail.com>
2018-10-25 15:13:49 +0000

    AI v AI masih bisa disempurnakan, kalo keblokir pilih random

--- game.js
+++ game.js
@@ -6,34 +6,173 @@ const PIHAK_HIJAU = 2;
 var giliran = PIHAK_MERAH;
 var nodeSekarang = new MinimaxTreeNode(PIHAK_BIRU,null,null,null,null,null);
 
-
+var gameOver = false;
 var nodeTerpilih = null;
 function klikPetak(event) {
-    var el = $(event.currentTarget);
-    var data = el.data();
-    let sukses = setMilikPetak(data.x, data.y, giliran, true);
-    if (!sukses) return;
-    remainingRed = CariJarak(merahStart, merahEnd, PIHAK_MERAH).cost;
-    remainingBlue = CariJarak(biruStart, biruEnd, PIHAK_BIRU).cost;
-    nodeSekarang = new MinimaxTreeNode(PIHAK_BIRU,null,null,null,null,null);
-    minimax_ab(nodeSekarang, 5, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 1);
-    nodeTerpilih = null;
-    nodeSekarang.anak.forEach(child => {
-        if(nodeSekarang.alpha == child.beta){
-            nodeTerpilih = child;
+    if(!gameOver){
+        var el = $(event.currentTarget);
+        var data = el.data();
+        let sukses = setMilikPetak(data.x, data.y, giliran, true);
+        gantiGiliran();
+        if (!sukses) return;
+        
+        nodeSekarang = new MinimaxTreeNode(PIHAK_BIRU,null,null,null,null,null);
+        minimax_ab(nodeSekarang, varDepth, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 1);
+        nodeTerpilih = null;
+        nodeSekarang.anak.forEach(child => {
+            if(nodeSekarang.alpha == child.beta){
+                nodeTerpilih = child;
+            }
+        });
+        if(nodeTerpilih == null){
+            var xx = null;
+            var yy = null;
+            do{
+                xx = Math.floor(Math.random() * besar);
+                yy = Math.floor(Math.random() * besar);
+            }while(returnJembatan(xx,yy,PIHAK_BIRU));
+            nodeTerpilih = new MinimaxTreeNode(PIHAK_BIRU,xx,yy,null,null,null);
         }
-    });
-    setMilikPetak(nodeTerpilih.x,nodeTerpilih.y,PIHAK_BIRU, true);
-    $("#merahR").text(remainingRed);
-    $("#biruR").text(remainingBlue);
-    
-    if (remainingRed == -1)
-        alert("Biru menang!");
-    else if (remainingBlue == -1)
-        alert("merah menang!");
-    gantiGiliran();
+        setMilikPetak(nodeTerpilih.x,nodeTerpilih.y,PIHAK_BIRU, true);
+        remainingRed = CariJarak(merahStart, merahEnd, PIHAK_MERAH).cost;
+        remainingBlue = CariJarak(biruStart, biruEnd, PIHAK_BIRU).cost;
+        $("#merahR").text(remainingRed);
+        $("#biruR").text(remainingBlue);
+        
+        if (remainingRed == -1){
+            gameOver = true;
+            alert("Biru menang!");
+        }
+        else if (remainingBlue == -1){
+            alert("merah menang!");
+            gameOver = true;
+        }
+        gantiGiliran();
+    }
+}
+var nodeMerah = null;
+var nodeBiru = null;
+var timerGame = null;
+function duaPlayerku(){
+    timerGame = setInterval(gameDuaPlayer,1000);
 }
 
+function gameDuaPlayer(){
+    nodeMerah = new MinimaxTreeNode(PIHAK_MERAH,null,null,null,null,null);
+    minimax_ab(nodeMerah, varDepth, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 1);
+    nodeTerpilih = null;
+    nodeMerah.anak.some(child => {
+        if(nodeMerah.alpha == child.beta){
+            nodeTerpilih = child;
+            return true;
+        }
+    });
+    if(nodeTerpilih == null){
+        var xx = null;
+        var yy = null;
+        do{
+            xx = Math.floor(Math.random() * besar);
+            yy = Math.floor(Math.random() * besar);
+        }while(returnJembatan(xx,yy,PIHAK_BIRU));
+        nodeTerpilih = new MinimaxTreeNode(PIHAK_MERAH,xx,yy,null,null,null);
+    }
+    setMilikPetak(nodeTerpilih.x,nodeTerpilih.y,PIHAK_MERAH, true);
+
+    nodeBiru = new MinimaxTreeNode(PIHAK_BIRU,null,null,null,null,null);
+    minimax_ab(nodeBiru, varDepth, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 1);
+    nodeTerpilih = null;
+    nodeBiru.anak.some(child => {
+        if(nodeBiru.alpha == child.beta){
+            nodeTerpilih = child;
+            return true;
+        }
+    });
+    if(nodeTerpilih == null){
+        var xx = null;
+        var yy = null;
+        do{
+            xx = Math.floor(Math.random() * besar);
+            yy = Math.floor(Math.random() * besar);
+        }while(returnJembatan(xx,yy,PIHAK_BIRU));
+        nodeTerpilih = new MinimaxTreeNode(PIHAK_BIRU,xx,yy,null,null,null);
+    }
+    setMilikPetak(nodeTerpilih.x,nodeTerpilih.y,PIHAK_BIRU, true);
+    remainingRed = CariJarak(merahStart, merahEnd, PIHAK_MERAH).cost;
+    remainingBlue = CariJarak(biruStart, biruEnd, PIHAK_BIRU).cost;
+    $("#merahR").text(remainingRed);
+    $("#biruR").text(remainingBlue);
+    if (remainingRed == -1){
+        gameOver = true;
+        alert("Biru menang!");
+    }
+    else if (remainingBlue == -1){
+        gameOver = true;
+        alert("merah menang!");
+    }
+    if(gameOver){
+        clearInterval(timerGame);
+    }
+}
+
+/*
+    enggak dipake,
+*/
+var cekStart = false;
+var cekEnd = false;
+var cekApakahMenang = true;
+function cekMenang(node){
+    cekStart = false;
+    cekEnd = false;
+    var nodeBuatCek = null;
+    if(node.giliran == PIHAK_MERAH){
+        merahStart.tetangga.forEach(petak => {
+            if (petak.milik == PIHAK_MERAH)
+                cekStart = true;
+        });
+        merahEnd.tetangga.forEach(petak => {
+            if (petak.milik == PIHAK_MERAH)
+                cekEnd = true;
+        });
+        nodeBuatCek = merahStart;
+    }
+    if(node.giliran == PIHAK_BIRU){
+        biruStart.tetangga.forEach(petak => {
+            if (petak.milik == PIHAK_MERAH)
+                cekStart = true;
+        });
+        biruEnd.tetangga.forEach(petak => {
+            if (petak.milik == PIHAK_MERAH)
+                cekEnd = true;
+        });
+        nodeBuatCek = biruStart;
+    }
+    if(cekStart && cekEnd){
+        cekApakahMenang = true;
+        recursiveCekMenang(nodeBuatCek);
+    }
+}
+/*
+    enggak dipake,
+*/
+function recursiveCekMenang(node){
+    var cekTetangga = false;
+    node.tetangga.some(petak => {
+        if(petak.milik == node.giliran){
+            cekTetangga = true;
+            return true;
+        }
+    });
+    if(cekTetangga){
+        node.tetangga.forEach(petak => {
+            if(petak.milik == node.giliran){
+                recursiveCekMenang(petak);
+            }
+        });
+    }
+    else{
+        cekApakahMenang = false;
+    }
+}
 /**
  * Mengeset petak pada posisi [x] dan [y] menjadi milik nya [pihak]
  * @param {int} x Posisi x petak
